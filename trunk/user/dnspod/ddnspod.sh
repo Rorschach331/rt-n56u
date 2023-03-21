643ea9797034b11d57df328665643ca8a27d1882
Rorschach <842007947@qq.com>
2023-03-21 09:43:52 +0000

    增加dnspod脚本

---
+++ trunk/user/dnspod/ddnspod.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+#
+
+# Import ardnspod functions
+. /etc/storage/ardnspod
+
+# Combine your token ID and token together as follows
+arToken="xxxx,token"
+
+# Web endpoint to be used for querying the public IPv6 address
+# Set this to override the default url provided by ardnspod
+arIp6QueryUrl="https://6.ipw.cn"
+
+# Return code when the last record IP is same as current host IP
+# Set this to a value other than 0 to distinguish with a successful ddns update
+# arErrCodeUnchanged=0
+
+# Place each domain you want to check as follows
+# you can have multiple arDdnsCheck blocks
+
+# IPv4:
+#arDdnsCheck "test.org" "subdomain"
+
+# IPv6:
+#arDdnsCheck "xxxx.com" "subdomain" 6
